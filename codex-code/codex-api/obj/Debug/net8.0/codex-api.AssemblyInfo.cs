908fc3ae8c1d14fcaccdc042a1a1583e3898d439
Miruna Georgescu <mirubi12@gmail.com>
2024-03-19 13:08:22 +0200

    Add Book model

--- codex-code/codex-api/obj/Debug/net8.0/codex-api.AssemblyInfo.cs
+++ codex-code/codex-api/obj/Debug/net8.0/codex-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codex-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865dcb91c2c2d16cee088de4ebe4e262a9f13e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f11cf7fa1d6ffcb53ca3dbd04130e682b9e606d")]
 [assembly: System.Reflection.AssemblyProductAttribute("codex-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codex-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
