9577e4a3636863f4dff9bcbba62e6318141df847
Miruna Georgescu <mirubi12@gmail.com>
2024-04-09 22:12:52 +0300

    Add index method for books

--- codex-code/Codex/Controllers/BooksController.cs
+++ codex-code/Codex/Controllers/BooksController.cs
@@ -5,7 +5,7 @@ using Microsoft.Identity.Client;
 
 namespace Codex.Controllers
 {
-    [Route("books")]
+    [Route("Books")]
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext database;
@@ -15,14 +15,22 @@ namespace Codex.Controllers
             database = context;
         }
 
-        public IActionResult Index()
+        private const int booksPerPage = 9;
+
+        // displaying all the books 
+        [HttpGet]
+        public IActionResult Index(int? page)
         {
+            //if theres no current page, we're on page 1
+            int pageNumber = page ?? 1; 
 
+            // the books that fit on a page
+            var paginatedBooks = paginateBooks(pageNumber);
 
-
-            return View();
+            return View(paginatedBooks);
         }
 
+        // show method for a certain book 
         [HttpGet("Show/{Id:int}")]
         public IActionResult Show(int id)
         {
@@ -30,6 +38,7 @@ namespace Codex.Controllers
             return View(book);
         }
 
+        // retrieving all the books from the database
         public IEnumerable<Book> GetAllBooks()
         {
             List<Book> allBooks = database.Books.ToList();
@@ -37,10 +46,34 @@ namespace Codex.Controllers
 
         }
 
+        // retrieving a certain book from the database knowing the id
         public Book GetBookByID(int id)
         {
             Book book = database.Books.FirstOrDefault(book => book.BookId == id);
             return book;
+        }
+
+        private IEnumerable<Book> paginateBooks(int pageNumber)
+        {
+            // getting all the books from the database
+            var allBooks = GetAllBooks();
+            double bookCount = allBooks.Count();
+
+            // calculating the total number of pages
+            int totalPages = (int)Math.Ceiling(bookCount / booksPerPage); 
+
+            // calculating how many books to skip based on the number of pages
+            int skip = (pageNumber - 1) * booksPerPage;
+
+            // out of the books we skip the ones already displayed on a previous page
+            // and return the ones that should be displayed on the current page
+            var paginatedBooks = allBooks.Skip(skip).Take(booksPerPage).ToList();
+
+            // seting ViewBag properties for pagination 
+            ViewBag.CurrPage = pageNumber;
+            ViewBag.TotalPages = totalPages;
+
+            return paginatedBooks;
         }
     }
 }
