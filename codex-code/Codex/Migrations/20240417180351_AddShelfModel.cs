3f5110702a31c6326f9c227ca998bbbe096edf8f
Miruna Georgescu <mirubi12@gmail.com>
2024-04-17 21:04:11 +0300

    Add Shelf Model

---
+++ codex-code/Codex/Migrations/20240417180351_AddShelfModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codex.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShelfModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shelfs",
+                columns: table => new
+                {
+                    ShelfId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shelfs", x => x.ShelfId);
+                    table.ForeignKey(
+                        name: "FK_Shelfs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shelfs_UserId",
+                table: "Shelfs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shelfs");
+        }
+    }
+}
--- codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
+++ codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b843dea03d774d85973519ec0c5dd50cb8e800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5329b3505c281da6fd51d7cacb69c96899f93ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
