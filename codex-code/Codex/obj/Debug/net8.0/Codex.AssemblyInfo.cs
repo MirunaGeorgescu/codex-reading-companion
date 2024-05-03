c76f1a1c59fb8506cc5c35702ba74dcb2d466576
Miruna Georgescu <mirubi12@gmail.com>
2024-05-03 13:25:45 +0300

    Profile carousel (bug fix)

--- codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
+++ codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672f1ec41b13fc4ede2949848880287a2052370e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3ac02450c29053f6608b5542bdae6b401ea812")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
