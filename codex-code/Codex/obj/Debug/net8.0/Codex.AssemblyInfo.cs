919213cb9f5c6e57068896b4a848eec8348a32a3
Miruna Georgescu <mirubi12@gmail.com>
2024-06-16 15:14:32 +0300

    Update home page

--- codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
+++ codex-code/Codex/obj/Debug/net8.0/Codex.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49cfe658b2612d2721269fb4007c936c1e0cc1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a002eeb5c36f3673c419d290cca21866545acfc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
